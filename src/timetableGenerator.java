28637b8ba7e722355e7eaa84d9773d5e79ab5779
CatchingSeven <maxheinrich113@gmail.com>
2026-05-14 16:55:07 +0930

    chore: Setting up initial project directory structure

---
+++ src/timetableGenerator.java
@@ -0,0 +1,6 @@
+/* Name is placeholder until we come up with a better one */
+public class timetableGenerator {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
