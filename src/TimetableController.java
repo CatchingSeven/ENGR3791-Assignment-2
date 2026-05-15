bd29083ec97451cf406f6913125e0e47b9509555
CatchingSeven <maxheinrich113@gmail.com>
2026-05-15 10:11:18 +0930

    chore: Added all classes from DMCD

---
+++ src/TimetableController.java
@@ -0,0 +1,7 @@
+
+public class TimetableController {
+    public static void main(String[] args){
+        System.out.println("Timetable Management System");
+    }
+
+}
--- src/timetableGenerator.java
+++
@@ -1,6 +0,0 @@
-/* Name is placeholder until we come up with a better one */
-public class timetableGenerator {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
